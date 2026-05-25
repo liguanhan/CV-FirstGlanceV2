9a758d1ac7b6b4a8af0dd135ce87572c68e6918b
liguanhan <923465337@qq.com>
2026-05-25 11:18:29 +0800

    Update dataset.py

--- classification/dataset.py
+++ classification/dataset.py
@@ -1,1 +1,32 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
 
+def get_cifar10_loaders(batch_size=128, num_workers=4, data_root='./data'):
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset = torchvision.datasets.CIFAR10(
+        root=data_root, train=True, download=True, transform=transform_train)
+    trainloader = DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+
+    testset = torchvision.datasets.CIFAR10(
+        root=data_root, train=False, download=True, transform=transform_test)
+    testloader = DataLoader(
+        testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    classes = ('plane', 'car', 'bird', 'cat', 'deer',
+               'dog', 'frog', 'horse', 'ship', 'truck')
+    
+    return trainloader, testloader, classes
